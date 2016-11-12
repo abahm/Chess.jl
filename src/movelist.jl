34e2c0afa0a87b880ba1c6865c71ba70e8872f91
Alan Bahm <alanbahm@gmail.com>
2016-11-12 15:01:38 -0800

    refactor

--- src/movelist.jl
+++ src/movelist.jl
@@ -1,10 +1,12 @@
 # movelist.jl
 
-const MAX_MOVES_LENGTH = 100
-const MAX_MOVES_DEPTH = 100
+const MAX_MOVES_PER_TURN = 100
+const MAX_PLYS_CAN_LOOK = 100
 
 type Movelist
-    moves::Array{Move, 2}
-    position::UInt8
-    depth::UInt8
+    moves::Array{Move, 2}  # (MAX_MOVES_PER_TURN x MAX_PLYS_CAN_LOOK)
+    move_n::UInt8
+    ply_n::UInt8
+
+    attacking_moves::Array{Move,1}
 end
